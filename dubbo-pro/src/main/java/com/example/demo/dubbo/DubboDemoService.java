65bc70287ac3374c6da88cfc2f7436e2de259be1
黄嘉锐 <huangjiarui@rapoo.com>
2019-01-03 11:27:15 +0800

    1

--- dubbo-pro/src/main/java/com/example/demo/dubbo/DubboDemoService.java
+++ dubbo-pro/src/main/java/com/example/demo/dubbo/DubboDemoService.java
@@ -1,13 +1,6 @@
 package com.example.demo.dubbo;
 
-/**
- * @Package org.jon.lv.dubbo.DubboDemoService
- * @Description: DubboDemoService
- * @Copyright: Copyright (c) 2016
- * Author lv bin
- * @date 2017/5/19 13:34
- * version V1.0.0
- */
+
 public interface DubboDemoService {
 
     void say(String words);
--- dubbo-pro/src/main/java/com/example/demo/dubbo/DubboDemoServiceImpl.java
+++ dubbo-pro/src/main/java/com/example/demo/dubbo/DubboDemoServiceImpl.java
@@ -3,14 +3,7 @@ package com.example.demo.dubbo;
 
 import com.alibaba.dubbo.config.annotation.Service;
 
-/**
- * @Package org.jon.lv.dubbo.DubboDemoServiceImpl
- * @Description: DubboDemoServiceImpl
- * @Copyright: Copyright (c) 2016
- * Author lv bin
- * @date 2017/5/19 13:35
- * version V1.0.0
- */
+
 @Service
 public class DubboDemoServiceImpl implements DubboDemoService{
     @Override
